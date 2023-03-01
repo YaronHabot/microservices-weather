d950062dd7afd50c57a20156b1833f81b4ac96d8
Yaron habot <yaron@bettertrucks.com>
2023-03-01 10:28:45 -0500

    Add initial dependensies

--- Cloudweather.Precipitation/Program.cs
+++ Cloudweather.Precipitation/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+//Listen for GET /observation and a zip code
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days ) =>
+{
+    return Results.Ok(zip);
+});
 
 app.Run();
